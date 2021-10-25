cef4a2b0665720e735ae51f68d257a44c13c0d06
karimkohel <kareemkohel@gmail.com>
2021-10-25 22:28:31 +0200

    general hygiene

--- MotionDetection.py
+++ MotionDetection.py
@@ -1,10 +1,9 @@
-import cv2 , time
+import cv2
 
-video = cv2.VideoCapture(0)
+video = cv2.VideoCapture('vtest.avi')
 
-frameWidth = int( video.get(cv2.CAP_PROP_FRAME_WIDTH))
-
-frameHeight = int ( video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+frameWidth = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+frameHeight = int (video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 fr, frame1 = video.read()
 fr, frame2 = video.read()
@@ -14,13 +13,9 @@ firstFrame = None
 while video.isOpened():
 
     diff = cv2.absdiff(frame1, frame2)
-
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-
     blur = cv2.GaussianBlur(gray,(5,5),0) #blurring the frame
-
     _, thresholdFrame = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY) # to select the first variable
-    
     thresholdFrame = cv2.dilate(thresholdFrame, None, iterations = 2) # apply layer of smoothining -- iteration is for how accurate the smoothing will be, if increased then the program will capture the noise also
    
     # contours is for the motion of any obj in the frame :
@@ -31,13 +26,12 @@ while video.isOpened():
 
     for contour in cntr:
 
-        (x,y,w,h) = cv2.boundingRect(contour)   # draw a bounding rectangle around the moved obj
-        
-        if cv2.contourArea(contour) < 1000 :
+        if cv2.contourArea(contour) < 1000 : # hygiene
             continue
 
+        (x,y,w,h) = cv2.boundingRect(contour)   # draw a bounding rectangle around the moved obj
         cv2.rectangle(frame1,(x,y),(x+w,y+h),(0,255,0),2)
-        cv2.drawContours(frame1, cntr, -1, (0, 255, 0), 2)
+        # cv2.drawContours(frame1, cntr, -1, (0, 255, 0), 2) 
 
     cv2.imshow('trial', frame1)
     frame1 = frame2
